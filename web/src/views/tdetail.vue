ab253cee8ad0a9f7514ba5901e5614b2c1ae7dff
zhaoyuanda <1426593075@qq.com>
2021-04-23 16:06:35 +0800

    4.23.1

--- web/src/views/tdetail.vue
+++ web/src/views/tdetail.vue
@@ -1,6 +1,49 @@
 <template>
   <main role="main">
-    <p>{{id}}</p>
+    <div class="coursecont">
+      <div class="coursepic tecti">
+        <div class="teaimg">
+          <img :src="teacher.image" width="150" />
+        </div>
+        <div class="teachtext">
+          <h3>
+            {{teacher.name}}&nbsp;&nbsp;
+            <strong>{{teacher.position}}</strong>
+          </h3>
+          <h4>个人简介</h4>
+          <p>{{teacher.intro}}</p>
+        </div>
+        <div class="clearh"></div>
+      </div>
+
+      <div class="clearh"></div>
+
+      <div class="tcourselist">
+        <h3 class="righttit" style="padding-left:50px;">在教课程</h3>
+        <ul class="tcourseul">
+          <li v-for="(course,index) in courses" :key="index">
+            <span class="courseimg tcourseimg">
+              <a href="#" target="_blank">
+                <img width="230" :src="course.image" />
+              </a>
+            </span>
+            <span class="tcoursetext">
+              <h4>
+                <a href="#" target="_blank" class="teatt">{{course.summary}}</a>
+              </h4>
+              <p class="teadec">{{course.name}}</p>
+              <div class="btn-group">
+          <router-link v-bind:to="'/detail?id=' + course.id" class="btn btn-outline-secondary">课程详情</router-link>
+        </div>
+            </span>
+            <div style="height:0" class="clearh"></div>
+          </li>
+          <div class="clearh"></div>
+        </ul>
+      </div>
+
+      <div class="clearh"></div>
+    </div>
   </main>
 </template>
 
@@ -10,7 +53,7 @@ export default {
   data: function() {
     return {
       id: "",
-      course: {},
+      courses: [],
       teacher: {}
     };
   },
@@ -31,8 +74,9 @@ export default {
             _this.id
         )
         .then(response => {
-          let resp = response.data;
-          console.log("老师信息", response);
+          let resp = response.data.content;
+          this.teacher = resp;
+          console.log("老师信息", this.teacher);
         });
     },
 
@@ -46,13 +90,17 @@ export default {
             _this.id
         )
         .then(response => {
-          let resp = response.data;
-          console.log("老师的课程信息", response);
+          let resp = response.data.content;
+          this.courses = resp;
+          console.log("老师的课程信息", this.courses);
         });
-    },
+    }
   }
 };
 </script>
 
 <style scoped>
+.coursepic.tecti {
+  height: 200px;
+}
 </style>
